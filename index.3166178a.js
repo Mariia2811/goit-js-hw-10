40eaa5fef8a8ef95e8b94658f564501e95817281
Mariia2811 <116021359+Mariia2811@users.noreply.github.com>
2023-05-01 13:05:32 +0000

    Deploying to gh-pages from @ Mariia2811/goit-js-hw-10@6348fec490c9bb94046737c38da7a742c7c4779a 🚀

---
+++ index.3166178a.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box").addEventListener("input",(e=>{})),name,fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
+//# sourceMappingURL=index.3166178a.js.map
